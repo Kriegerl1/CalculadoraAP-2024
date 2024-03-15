e853e883cab238743baf9905e565b1fc88c79696
Leonardo Rodrigues <lthkrieger@gmail.com>
2024-03-15 19:37:17 -0300

    Fixed Menu, Added function, Improved UX, Push to deliver

--- Program.cs
+++ Program.cs
@@ -1,133 +1,102 @@
-﻿namespace CalculadoraConsoleApp
+﻿using System;
+
+namespace CalculadoraConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Calculadora Tabajara 2024\nSelecione uma opção:");
-            Console.WriteLine("1- Adição.\n2- Subtração.\n3- Multiplicação.\n4- Divisão.\n5- Fechar.");
-
-            bool controlador;
-
-            string operador = Console.ReadLine();
-
-
             do
             {
                 Console.Clear();
+                Console.WriteLine("Calculadora Tabajara | Academia de programação 2024!\n");
+                Console.WriteLine("1- Adição.\n2- Subtração.\n3- Multiplicação.\n4- Divisão.\n5- Fechar.");
+
+                string operador = Console.ReadLine();
 
                 switch (operador)
                 {
                     case "1":
-                        Console.Clear();
-                        Console.WriteLine("Adição\nDigite o primeiro número: ");
-
-                        string primeiroNumeroString = Console.ReadLine();
-                        decimal primeiroNumero = Convert.ToDecimal(primeiroNumeroString);
-                        Console.WriteLine("Digite o segundo número: ");
-                        string segundoNumeroString = Console.ReadLine();
-                        decimal segundoNumero = Convert.ToDecimal(segundoNumeroString);
-
-                        decimal resultado = primeiroNumero + segundoNumero;
-                        Console.WriteLine("O resultado da sua operação é: " + resultado.ToString("F"));
-
-                        Console.WriteLine("Deseja continuar? (S/N)");
-                        string repeat = Console.ReadLine();
-                        Console.ReadLine();
-
-                        if (repeat == "S" || repeat == "s")
-                        {
-                            operador = "1";
-                        }
-                        else
-                        {
-                            return;
-                        }
+                        Adicao();
                         break;
 
                     case "2":
-                        Console.Clear();
-                        Console.WriteLine("Subtração\nDigite o primeiro número: ");
-
-                        primeiroNumeroString = Console.ReadLine();
-                        primeiroNumero = Convert.ToDecimal(primeiroNumeroString);
-                        Console.WriteLine("Digite o segundo número: ");
-                        segundoNumeroString = Console.ReadLine();
-                        segundoNumero = Convert.ToDecimal(segundoNumeroString);
-
-                        resultado = primeiroNumero - segundoNumero;
-                        Console.WriteLine("O resultado da sua operação é: " + resultado.ToString("F"));
-
-                        Console.WriteLine("Deseja continuar? (S/N)");
-                        repeat = Console.ReadLine();
-                        Console.ReadLine();
-
-                        if (repeat == "S" || repeat == "s")
-                        {
-                            operador = "2";
-                        }
-                        else
-                        {
-                            return;
-                        }
+                        Subtracao();
                         break;
 
                     case "3":
-                        Console.Clear();
-                        Console.WriteLine("Multiplicação\nDigite o primeiro número: ");
-
-                        primeiroNumeroString = Console.ReadLine();
-                        primeiroNumero = Convert.ToDecimal(primeiroNumeroString);
-                        Console.WriteLine("Digite o segundo número: ");
-                        segundoNumeroString = Console.ReadLine();
-                        segundoNumero = Convert.ToDecimal(segundoNumeroString);
-
-                        resultado = primeiroNumero * segundoNumero;
-                        Console.WriteLine("O resultado da sua operação é: " + resultado.ToString("F"));
-
-                        Console.WriteLine("Deseja continuar? (S/N)");
-                        repeat = Console.ReadLine();
-                        Console.ReadLine();
-
-                        if (repeat == "S" || repeat == "s")
-                        {
-                            operador = "3";
-                        }
-                        else
-                        {
-                            return;
-                        }
+                        Multiplicacao();
                         break;
 
                     case "4":
-                        Console.Clear();
-                        Console.WriteLine("Divisão\nDigite o primeiro número: ");
+                        Divisao();
+                        break;
 
-                        primeiroNumeroString = Console.ReadLine();
-                        primeiroNumero = Convert.ToDecimal(primeiroNumeroString);
-                        Console.WriteLine("Digite o segundo número: ");
-                        segundoNumeroString = Console.ReadLine();
-                        segundoNumero = Convert.ToDecimal(segundoNumeroString);
+                    case "5":
+                        return;
 
-                        resultado = primeiroNumero / segundoNumero;
-                        Console.WriteLine("O resultado da sua operação é: " + resultado.ToString("F"));
-
-                        Console.WriteLine("Deseja continuar? (S/N)");
-                        repeat = Console.ReadLine();
-                        Console.ReadLine();
-
-                        if (repeat == "S" || repeat == "s")
-                        {
-                            operador = "4";
-                        }
-                        else
-                        {
-                            return;
-                        }
+                    default:
+                        Console.WriteLine("Opção inválida. Por favor, selecione uma opção válida.");
                         break;
                 }
-            } while (controlador = true);
+
+                Console.WriteLine("\nDeseja realizar outra operação? (S/N)");
+            } while (Console.ReadLine() == "S" || Console.ReadLine() == "s");
+        }
+
+        static void Adicao()
+        {
+            Console.WriteLine("Adição\nDigite o primeiro número:");
+            double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = primeiroNumero + segundoNumero;
+            Console.WriteLine($"O resultado da adição é: {resultado.ToString("F")}");
+        }
+
+        static void Subtracao()
+        {
+            Console.WriteLine("Subtração\nDigite o primeiro número:");
+            double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = primeiroNumero - segundoNumero;
+            Console.WriteLine($"O resultado da subtração é: {resultado.ToString("F")}");
+        }
+
+        static void Multiplicacao()
+        {
+            Console.WriteLine("Multiplicação\nDigite o primeiro número:");
+            double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = primeiroNumero * segundoNumero;
+            Console.WriteLine($"O resultado da multiplicação é: {resultado.ToString("F")}");
+        }
+
+        static void Divisao()
+        {
+            Console.WriteLine("Divisão\nDigite o primeiro número:");
+            double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+            if (segundoNumero == 0)
+            {
+                Console.WriteLine("A operação é inválida, o divisor não pode ser igual a zero!");
+            }
+            else
+            {
+                double resultado = primeiroNumero / segundoNumero;
+                Console.WriteLine($"O resultado da divisão é: {resultado.ToString("F")}");
+            }
         }
     }
 }
